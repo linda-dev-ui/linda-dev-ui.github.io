7b25bfaf94d7c364819f29ca3df52ad0d10d1dd5
migzam10 <mz39729@gmail.com>
2025-11-06 14:54:38 -0500

    cambio de nombre

--- anim.js
+++ anim.js
@@ -62,10 +62,10 @@ setInterval(updateLyrics, 1000);
 function ocultarTitulo() {
   var titulo = document.querySelector(".titulo");
   titulo.style.animation =
-    "fadeOut 3s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
+    "fadeOut 1s ease-in-out forwards"; /* Duración y función de temporización de la desaparición */
   setTimeout(function () {
     titulo.style.display = "none";
-  }, 3000); // Espera 3 segundos antes de ocultar completamente
+  }, 1000); // Espera 3 segundos antes de ocultar completamente
 }
 
 // Llama a la función después de 216 segundos (216,000 milisegundos)
